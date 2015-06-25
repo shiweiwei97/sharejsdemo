1369a8f41564b723b4941ec0a98943c64fae4582
Weiwei Shi <shiweiwei97@gmail.com>
2015-06-25 15:03:24 +0800

    sharejs demo

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
+var sharejs = require('share').server;
+var exphbs = require('express-handlebars');
+
+app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send(cool());
+app.get('/', function(req, res) {
+  res.render('index');
 });
+
+var options = {db: {type: 'none'}};
+sharejs.attach(app, options);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
